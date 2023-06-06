21cb33a7ac2860b636d5cce7a1bfcb0237e7606e
Johnnie Walker <jw12866@gmail.com>
2023-06-06 14:05:52 -0400

    added unbilled revenue capability

--- main.py
+++ main.py
@@ -29,12 +29,13 @@ def main():
     # Create a recurring RevenueSegment for the Contract
     revenue_segment = RevenueSegment(
         contract=contract,
-        delay_mths=3,
-        length_mths=5,
-        is_recurring=1,
+        delay_rev_start_mths=5,
+        delay_inv_from_rev_mths=-3,
+        length_rev_mths=4,
         amount=100.00,
         type='Product',
         name='Recurring License Revenue',
+        invoice_schedule='Upfront',
     )
 
     # Add the revenue segment to the contract
@@ -43,10 +44,10 @@ def main():
     # Commit the session to save these objects to the database
     session.commit()
 
-    num_months = 12
+    num_months = 13
 
     # Initialize dataframe with zero values
-    df = pd.DataFrame(0, index=range(1, num_months), columns=['TCV', 'DefRev', 'Rev', 'AR', 'Cash'])
+    df = pd.DataFrame(0, index=range(1, num_months), columns=['TCV', 'DefRev', 'UnbilledRev', 'Rev', 'AR', 'Cash'])
 
     # Query all objects
     revenue_segments = session.query(RevenueSegment).all()
@@ -56,18 +57,36 @@ def main():
     for segment in revenue_segments:
         total_contract_value = segment.get_total_revenue()
         booked_month = segment.contract.booked_month
-        df.loc[booked_month, 'TCV'] += total_contract_value  # add to the current value
-        for i in range(booked_month + segment.delay_mths, booked_month + segment.delay_mths + segment.length_mths):
+
+        # Post the total contract value to the booked month
+        df.loc[booked_month, 'TCV'] += total_contract_value
+
+        # Record revenue for each month
+        for i in range(booked_month + segment.delay_rev_start_mths, booked_month + segment.delay_rev_start_mths + segment.length_rev_mths):
             df.loc[i, 'Rev'] += segment.amount
-            for j in range(i, num_months):
-                df.loc[j, 'DefRev'] -= segment.amount
+            if segment.delay_inv_from_rev_mths > 0 and i < booked_month + segment.delay_rev_start_mths + segment.delay_inv_from_rev_mths:
+                for j in range(i, num_months):
+                    df.loc[j, 'UnbilledRev'] += segment.amount
+            else:
+                for j in range(i, num_months):
+                    df.loc[j, 'DefRev'] -= segment.amount
 
     for invoice in invoices:
+        print('processing invoice')
         issue_month = invoice.issue_mth
         paid_month = invoice.issue_mth + invoice.mths_payable
+
         for i in range(issue_month, num_months):
             df.loc[i, 'AR'] += invoice.amount
-            df.loc[i, 'DefRev'] += invoice.amount
+            if segment.delay_inv_from_rev_mths > 0:
+                if i > booked_month + segment.delay_rev_start_mths + segment.length_rev_mths:
+                    df.loc[i, 'UnbilledRev'] -= invoice.amount
+                else:
+                    df.loc[i, 'UnbilledRev'] -= segment.delay_inv_from_rev_mths * segment.amount
+                    df.loc[i, 'DefRev'] += (segment.length_rev_mths - segment.delay_inv_from_rev_mths) * segment.amount
+            else:
+                df.loc[i, 'DefRev'] += invoice.amount    
+
         for i in range(paid_month, num_months):
             df.loc[i, 'Cash'] += invoice.amount
             df.loc[i, 'AR'] -= invoice.amount
--- models.py
+++ models.py
@@ -55,13 +55,14 @@ class RevenueSegment(Base):
     __tablename__ = 'revenue_segments'
 
     id = Column(Integer, primary_key=True)
-    delay_mths = Column(Integer) # number of months from booked date to start of revenue
-    length_mths = Column(Integer) # should be 1 for one-time revenue
+    delay_rev_start_mths = Column(Integer) # number of months from booked date to start of revenue
+    length_rev_mths = Column(Integer) # should be 1 for one-time revenue
+    delay_inv_from_rev_mths = Column(Integer) # number of months from start of revenue to invoice, can be negative
     amount = Column(Integer) # one-time amount or recurring amount
     name = Column(String) 
     type = Column(Enum('Service', 'Product', name='revenue_segment_type'))
     invoice_schedule = Column(Enum('Upfront', 'Monthly', name='invoice_schedule'))
-    is_recurring = Column(Integer) # 1 for recurring, 0 for one-time
+    _unbilled_balance = Column(Integer) # unbilled balance for this revenue segment (private variable)
     
     # Parent is Contract
     contract_id = Column(Integer, ForeignKey('contracts.id'))
@@ -70,21 +71,21 @@ class RevenueSegment(Base):
     # Children are Invoices
     invoices = relationship("Invoice", back_populates="revenue_segment")
 
-    def __init__(self, contract, delay_mths, length_mths, amount, name, type, is_recurring, invoice_schedule='Upfront'):
+    def __init__(self, contract, amount, name, type, delay_rev_start_mths, length_rev_mths, delay_inv_from_rev_mths=0, invoice_schedule='Upfront'):
         self.contract = contract
-        self.delay_mths = delay_mths
-        self.length_mths = length_mths
+        self.delay_rev_start_mths = delay_rev_start_mths 
+        self.length_rev_mths = length_rev_mths 
+        self.delay_inv_from_rev_mths = delay_inv_from_rev_mths
         self.amount = amount
         self.name = name
         self.type = type
-        self.is_recurring = is_recurring
         self.invoice_schedule = invoice_schedule
 
         # Create invoice(s) for this revenue segment
         if self.invoice_schedule == 'Upfront':
             invoice = Invoice(
-                months_delay=self.delay_mths,
-                amount=self.amount * self.length_mths,
+                months_delay=self.delay_rev_start_mths + self.delay_inv_from_rev_mths,
+                amount=self.amount * self.length_rev_mths,
                 mths_payable=1,
                 contract_booked_month=self.contract.booked_month
             )
@@ -92,7 +93,7 @@ class RevenueSegment(Base):
         elif self.invoice_schedule == 'Monthly':
             for i in range(self.length_mths):
                 invoice = Invoice(
-                    months_delay=self.delay_mths + i,
+                    months_delay=self.delay_rev_start_mths + delay_inv_from_rev_mths + i,
                     amount=self.amount,
                     mths_payable=1,
                     contract_booked_month=self.contract.booked_month
