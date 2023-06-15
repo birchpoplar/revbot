da936145376b91b37275c61207a9494a868d966d
Johnnie Walker <jw12866@gmail.com>
2023-06-15 12:51:57 -0400

    hardcoding heroku url into client script

--- client.py
+++ client.py
@@ -1,13 +1,22 @@
 import requests
 import pandas as pd
 import json
+import os
 from revbot.services.display_df import display_df
+
+
+
+# Set URL to the Heroku app if running in production, otherwise set to localhost
+#if os.getenv('FLASK_CONFIG') == 'ProductionConfig':
+url = 'https://bp-revbot-c76c85faef18.herokuapp.com'
+#else:
+#    url = 'http://localhost:5000'
 
 def create_customer():
     data = {
         "name": "Test Customer"
     }
-    response = requests.post('http://localhost:5000/customers', json=data)
+    response = requests.post(url + '/customers', json=data)
     if response.status_code == 200:
         print("Customer created successfully : id " + str(response.json()['id']))
     else:
@@ -19,7 +28,7 @@ def create_contract(customer_id):
         "customer_id": customer_id,
         "booked_month": 10,
     }
-    response = requests.post('http://localhost:5000/contracts', json=data)
+    response = requests.post(url + '/contracts', json=data)
     if response.status_code == 200:
         print("Contract created successfully : id " + str(response.json()['id']))
     else:
@@ -37,7 +46,7 @@ def create_revenue_segment(contract_id):
         "delay_inv_from_rev_mths" : -3,
         "invoice_schedule" : 'Monthly' 
     }
-    response = requests.post('http://localhost:5000/revenuesegments', json=data)
+    response = requests.post(url + '/revenuesegments', json=data)
     if response.status_code == 200:
         print("Revenue segment created successfully : id " + str(response.json()['id']))
     else:
@@ -45,7 +54,7 @@ def create_revenue_segment(contract_id):
     return response.json()
 
 def get_dataframe():
-    response = requests.get('http://localhost:5000/dataframe')
+    response = requests.get(url + '/dataframe')
     data = response.json()
 
     df = pd.DataFrame(data=data['data'], index=data['index'], columns=data['columns'])
@@ -54,7 +63,7 @@ def get_dataframe():
     display_df(df)
 
 def clear_database():
-    response = requests.delete('http://localhost:5000/clear_database')
+    response = requests.delete(url + '/clear_database')
     if response.status_code == 200:
         print("Database cleared successfully")
     else:
@@ -66,13 +75,17 @@ if __name__ == "__main__":
     # clear_database()
     
     # Create customer
+    print('Attempting to create customer')
     customer = create_customer()
         
     # Create contract
+    print('Attempting to create contract')
     contract = create_contract(customer['id'])
     
     # Create revenue segment
+    print('Attempting to create revenue segment')
     create_revenue_segment(contract['id'])
     
     # Get and display dataframe
+    print('Attempting to get dataframe')
     get_dataframe()
