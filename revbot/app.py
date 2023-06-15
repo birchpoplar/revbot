7287197ef63ebbfcc99709b847d6a472b59d3122
Johnnie Walker <jw12866@gmail.com>
2023-06-15 07:05:13 -0400

    fixing to remove create_app()

--- revbot/app.py
+++ revbot/app.py
@@ -7,26 +7,23 @@ from revbot.routes.revenuesegment_routes import revenuesegment_routes
 from revbot.routes.dataframe_routes import dataframe_routes
 from revbot.routes.clear_routes import clear_routes
 
-def create_app():
-    config_class = os.getenv('FLASK_CONFIG', 'Config')
-    app = Flask(__name__)
-    app.config.from_object(f'config.{config_class}')
+config_class = os.getenv('FLASK_CONFIG', 'Config')
+app = Flask(__name__)
+app.config.from_object(f'config.{config_class}')
 
-    reset_and_init_db, create_session, remove_session = create_engine_and_session(app)
-    reset_and_init_db()
+reset_and_init_db, create_session, remove_session = create_engine_and_session(app)
+reset_and_init_db()
     
-    app.register_blueprint(customer_routes)
-    app.register_blueprint(contract_routes)
-    app.register_blueprint(revenuesegment_routes)
-    app.register_blueprint(dataframe_routes)
-    app.register_blueprint(clear_routes)
+app.register_blueprint(customer_routes)
+app.register_blueprint(contract_routes)
+app.register_blueprint(revenuesegment_routes)
+app.register_blueprint(dataframe_routes)
+app.register_blueprint(clear_routes)
 
-    @app.before_request
-    def before_request():
-        g.db_session = create_session()
+@app.before_request
+def before_request():
+    g.db_session = create_session()
 
-    @app.teardown_request
-    def teardown_request(exception=None):
-        remove_session(g.db_session)
-
-    return app
+@app.teardown_request
+def teardown_request(exception=None):
+    remove_session(g.db_session)
--- run.py
+++ run.py
@@ -1,6 +1,4 @@
-from revbot.app import create_app
-
-app = create_app()
+from revbot.app import app
 
 if __name__ == "__main__":
     app.run(debug=True)
